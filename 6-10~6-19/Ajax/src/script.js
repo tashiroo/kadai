e35b1340aad49dc5aca5651020d02f56f9e74a18
taashiroo <ing631tom@outlook.jp>
2020-06-15 22:48:42 +0900

    wheaterCard

--- 6-10~6-19/Ajax/src/script.js
+++ 6-10~6-19/Ajax/src/script.js
@@ -1,25 +1,36 @@
 const divElement = document.getElementById("root");
 
-fetch(
-  "http://api.openweathermap.org/data/2.5/weather?q=tokyo&appid=2bea3d3ebe2c30f886caa303394d20d7"
-).then((response) => {
-  response.json().then((json) => {
-    console.log("json", json);
-    console.log("json.name", json.name);
-    let kerubin = json.main.temp;
-    let sessi = kerubin - 273.15;
-    console.log(sessi);
-  });
-});
 
-const kion = document.createElement("p");
-kion.innerText = JSON[0];
+const kion = document.createElement("h1");
+
+const tenki = document.createElement("p");
+
+const youbi = document.createElement("h2");
+youbi.innerText = "Thursday";
+
+const syutokujikan = document.createElement("p");
+syutokujikan.innerText = "Mach 1st 1:00 pm"
+syutokujikan.classList.add("time");
 
 const weatherIcon = document.createElement("img");
 weatherIcon.src = "http://openweathermap.org/img/wn/04n@2x.png";
 
 const weatherCord = document.createElement("span");
-weatherCord.appendChild(kion);
+weatherCord.appendChild(youbi);
+weatherCord.appendChild(syutokujikan);
 weatherCord.appendChild(weatherIcon);
+weatherCord.appendChild(kion);
+weatherCord.appendChild(tenki);
 divElement.appendChild(weatherCord);
+weatherCord.classList.add("weatherCord");
 
+fetch(
+  "http://api.openweathermap.org/data/2.5/weather?q=tokyo&appid=2bea3d3ebe2c30f886caa303394d20d7"
+).then((response) => {
+  response.json().then((json) => {
+    console.log("json", json);
+    kion.innerText = Math.floor(json.main.temp - 273.15)+"℃";
+    tenki.innerText = json.weather[0].description;
+   
+  });
+});
