7993a15f3fcfd512cd2a1edf760d73abb51d522a
taashiroo <ing631tom@outlook.jp>
2020-06-25 00:28:37 +0900

    6/24の学習内容,兼6/24~6/29の学習予定

--- 6-10~6-19/Ajax/src/script.js
+++ 6-10~6-19/Ajax/src/script.js
@@ -1,36 +1,39 @@
+const BASE_URL = "http://api.openweathermap.org/data/2.5/weather";
+const APP_ID = "2bea3d3ebe2c30f886caa303394d20d7";
+
+function generateIconURL(iconId) {
+  return `http://openweathermap.org/img/wn/${iconId}@2x.png`
+}
+
+const place = "london"
+
 const divElement = document.getElementById("root");
-
-
 const kion = document.createElement("h1");
-
 const tenki = document.createElement("p");
-
 const youbi = document.createElement("h2");
-youbi.innerText = "Thursday";
-
 const syutokujikan = document.createElement("p");
-syutokujikan.innerText = "Mach 1st 1:00 pm"
-syutokujikan.classList.add("time");
-
 const weatherIcon = document.createElement("img");
-weatherIcon.src = "http://openweathermap.org/img/wn/03n@2x.png";
-
 const weatherCord = document.createElement("span");
+
 weatherCord.appendChild(youbi);
 weatherCord.appendChild(syutokujikan);
 weatherCord.appendChild(weatherIcon);
 weatherCord.appendChild(kion);
 weatherCord.appendChild(tenki);
 divElement.appendChild(weatherCord);
+
+const now = new Date();
+syutokujikan.innerText = `${now.getMonth()}/${now.getDay()}`
+syutokujikan.classList.add("time");
 weatherCord.classList.add("weatherCord");
 
-fetch(
-  "http://api.openweathermap.org/data/2.5/weather?q=tokyo&appid=2bea3d3ebe2c30f886caa303394d20d7"
-).then((response) => {
-  response.json().then((json) => {
-    console.log("json", json);
-    kion.innerText = Math.floor(json.main.temp - 273.15)+"℃";
-    tenki.innerText = json.weather[0].description;
-   
-  });
-});
+async function callback(response) {
+  const json = await response.json();
+
+  kion.innerText = Math.floor(json.main.temp - 273.15)+"℃";
+  youbi.innerText = json.name;
+  tenki.innerText = json.weather[0].description;
+  weatherIcon.src = generateIconURL(json.weather[0].icon)
+}
+
+fetch(`${BASE_URL}?q=${place}&appid=${APP_ID}`).then(callback);
